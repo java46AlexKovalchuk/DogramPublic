4f6911f94e507b3f8a5fffe583a1d2a407b49160
AlexKovalchuk <kovalchuk111@gmail.com>
2022-01-02 02:29:55 +0200

    hw-7

---
+++ src/index.js
@@ -0,0 +1,15 @@
+console.log("script launched");
+let detailsImage = document.querySelector(".details-images"); 
+let detailsTitle = document.querySelector(".details-title");  
+let anchors = document.querySelectorAll(".thumbnails-anchor"); //all html elements belonging to the class thumbnails-anchor
+for(let i = 0; i < anchors.length; i++)  {
+    anchors[i].addEventListener("click", function(event) {
+    event.preventDefault(); //cancelling default behavior of anchor element hitting
+    setDetails(anchors[i]); //setDetail function call with passing reference to appropriate anchor
+    })
+}
+function setDetails(anchor) {
+    console.log("anchor element was pressed", anchor);
+    detailsImage.setAttribute("src", anchor.getAttribute("href"));
+    detailsTitle.textContent = anchor.getAttribute("data-details-title")
+}
