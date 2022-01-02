dff93a4c2cd3d011f1bfb3568b8c6f62935fde80
AlexKovalchuk <kovalchuk111@gmail.com>
2022-01-02 19:07:31 +0200

    hw-7_Add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.log("script launched");
 let detailsImage = document.querySelector(".details-images"); 
 let detailsTitle = document.querySelector(".details-title");  
+//let thumbnailsName = document.querySelectorAll(".thumbnails-title");
 let anchors = document.querySelectorAll(".thumbnails-anchor"); //all html elements belonging to the class thumbnails-anchor
 for(let i = 0; i < anchors.length; i++)  {
     anchors[i].addEventListener("click", function(event) {
@@ -11,5 +12,6 @@ for(let i = 0; i < anchors.length; i++)  {
 function setDetails(anchor) {
     console.log("anchor element was pressed", anchor);
     detailsImage.setAttribute("src", anchor.getAttribute("href"));
-    detailsTitle.textContent = anchor.getAttribute("data-details-title")
+   // detailsTitle.textContent = anchor.getAttribute("data-details-title"  );
+    detailsTitle.textContent = anchor.textContent+ ": " + anchor.getAttribute("data-details-title"  );
 }
