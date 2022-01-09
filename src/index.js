9764714604caaef9f83121ab728919580cc16fa6
AlexKovalchuk <kovalchuk111@gmail.com>
2022-01-09 03:50:50 +0200

    hw-9

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let detailsImage = document.querySelector(".details-images");
 let detailsTitle = document.querySelector(".details-title");  
 let mainContentEl = document.querySelector(".main-content");
 let selectedItem;
+let centrSound = document.querySelector(".central-sound");
 let anchors = document.querySelectorAll(".thumbnails-anchor"); //all html elements belonging to the class thumbnails-anchor
 for(let i = 0; i < anchors.length; i++)  {
     anchors[i].addEventListener("click", function(event) {
@@ -17,6 +18,7 @@ function setDetails(anchor) {
     console.log("anchor element was pressed", anchor);
     let hrefValue = anchor.getAttribute("href");    
     detailsImage.setAttribute("src", hrefValue);
+
     anchor.parentElement.classList.add("selected");
     if(selectedItem){
         selectedItem.classList.remove("selected")
@@ -25,17 +27,38 @@ function setDetails(anchor) {
     //get element with thumbnail-title inside the given anchor
     let thumbnailsTitleSelector = `[href="${hrefValue}"] .thumbnails-title`;
     let thumbnailsTitleEl = document.querySelector (thumbnailsTitleSelector);
+    
+    let thumbnailsSoundSelector = `[href="${hrefValue}"] .thumbnails-sound`;
+    let thumbnailsSoundElement = document.querySelector(thumbnailsSoundSelector);
+    let addressBarkl = thumbnailsSoundElement.getAttribute("src");
     //dog name exist inside thumbnailsTitleEl.textContent
     detailsTitle.textContent = `${thumbnailsTitleEl.textContent}: ${anchor.getAttribute('data-details-title')} ` ;
-
+    centrSound.setAttribute("src", addressBarkl);
+    centrSound.muted = false;
+    setTimeout(addMute, 900);
+    
+    function addMute() {
+        centrSound.muted = true;
+    }
 }   
 
 function showDetails()  {
     mainContentEl.classList.remove('hidden');
+    detailsImage.parentElement.classList.add('is-tiny');
+    setTimeout(removeIsTiny, 1);
+    
+   
 }
+
+function removeIsTiny() {
+    detailsImage.parentElement.classList.remove('is-tiny'); 
+}
+
 function hideDetails() {
    mainContentEl.classList.add('hidden');
+   if(selectedItem){
    selectedItem.classList.remove("selected");
+   }
 } 
    
 
